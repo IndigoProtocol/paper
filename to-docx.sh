d36ec3befa366eb0b49d0417c179d97a048246fa
Cody Butz <codyjbutz@gmail.com>
2023-06-01 14:00:57 -0400

    Remove volatility factor from SP rewards

--- to-docx.sh
+++ to-docx.sh
@@ -6,15 +6,15 @@ set -e
 
 # Convert tabularx tables to longtables so pandoc can understand
 sed 's/\\end{tabularx}/\\end{longtable}/g' "${1:-main.tex}" > main.tex.tmp.pandoc
-sed -i 's/\\begin{tabularx}{\\linewidth}{/\\begin{longtable}[]{/g' main.tex.tmp.pandoc
-sed -i 's/\\begin{longtable}.*/\L&/g' main.tex.tmp.pandoc
+sed -i '' -e 's/\\begin{tabularx}{\\linewidth}{/\\begin{longtable}[]{/g' main.tex.tmp.pandoc
+sed -i '' -e 's/\\begin{longtable}.*/\L&/g' main.tex.tmp.pandoc
 
 # Convert formulas
-sed -i 's/\\genfrac{}{}{}{}/\\frac/g' main.tex.tmp.pandoc
-sed -i 's/\\cfrac/\\frac/g' main.tex.tmp.pandoc
-sed -i 's/\\raisebox//g' main.tex.tmp.pandoc
-sed -i 's/{\$/{/g' main.tex.tmp.pandoc
-sed -i 's/\$}/}/g' main.tex.tmp.pandoc
+sed -i '' -e 's/\\genfrac{}{}{}{}/\\frac/g' main.tex.tmp.pandoc
+sed -i '' -e 's/\\cfrac/\\frac/g' main.tex.tmp.pandoc
+sed -i '' -e 's/\\raisebox//g' main.tex.tmp.pandoc
+sed -i '' -e 's/{\$/{/g' main.tex.tmp.pandoc
+sed -i '' -e 's/\$}/}/g' main.tex.tmp.pandoc
 
 # Process with pandoc
 pandoc main.tex.tmp.pandoc -f latex -t docx -o main.docx
